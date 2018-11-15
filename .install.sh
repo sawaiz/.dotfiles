52cf5f862b8f4b5a2582c453d1c31d0cc6e1dbc9
sawaiz <github@sawaizsyed.com>
2018-11-15 20:29:40 +0100

    fixed a few issues in install script and split install script

--- .install.sh
+++ .install.sh
@@ -1,8 +1,9 @@
+#!/bin/bash
 # Single Install script, built for WSL.
 
 #Check if running as root
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
+if [[ $(id -u) -ne 0 ]]; then
+  echo "Please run as root"
   exit
 fi
 
@@ -11,17 +12,17 @@ apt-get update && apt-get -y upgrade
 
 # Pull configuration from git
 sudo apt-get install -y build-essential checkinstall git zsh
-alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 sudo -u $SUDO_USER git clone --bare https://github.com/Sawaiz/.dotfiles.git $HOME/.dotfiles
 cd $HOME
-dotfiles checkout
-dotfiles submodule update --init --recursive
-dotfiles remote set-url origin git@github.com:Sawaiz/.dotfiles.git
+sudo -u $SUDO_USER /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME checkout
+sudo -u $SUDO_USER /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME submodule update --init --recursive
+sudo -u $SUDO_USER /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME remote set-url origin git@github.com:Sawaiz/.dotfiles.git
 
 # Request user to paste RSA Key
-echo "Replace with id_rsa" > $HOME/.ssh/id_rsa
-"${EDITOR:-nano}" ~/.ssh/id_rsa
-sudo -u $SUDO_USER chmod 600 ~/.ssh/*
+sudo -u $SUDO_USER mkdir -p $HOME/.ssh
+sudo -u $SUDO_USER echo "Replace with id_rsa" > $HOME/.ssh/id_rsa
+sudo -u $SUDO_USER "${EDITOR:-nano}" $HOME/.ssh/id_rsa
+sudo -u $SUDO_USER chmod 600 $HOME/.ssh/*
 
 # Install tmux
 sudo apt-get install -y libevent-dev ncurses-dev autoconf
@@ -33,35 +34,3 @@ cd tmux-2.6
 make -j
 make install
 
-# Install NPM and some global node modules
-curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
-sudo apt-get install -y nodejs
-npm install -g browser-sync
-sudo npm install -g browser-sync
-
-# Install ROOT
-sudo apt-get install -y build-essential checkinstall
-sudo apt-get install -y dpkg-dev g++ gcc binutils libx11-dev libxpm-dev libxft-dev libxext-dev
-sudo apt-get install -y xorg
-sudo apt-get install -y git
-sudo apt-get install -y python-dev
-
-## Install CMAKE
-cd /tmp
-wget http://www.cmake.org/files/v3.9/cmake-3.9.0.tar.gz
-tar xf cmake-3.9.0.tar.gz
-cd cmake-3.9.0
-./configure
-make -j
-make install
-cd ..
-rm -r cmake-3.9.0.tar.gz cmake-3.9.0
-
-## Create Install location
-mkdir /usr/local/root
-cd /usr/local/root
-
-## Clone and build
-git clone http://root.cern.ch/git/root.git
-cmake ./root
-cmake --build . -- -j
