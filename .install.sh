a6e40d16331f5b1635c25e8887c0cd3f14693d1d
Sawaiz Syed <github@sawaizsyed.com>
2020-07-04 15:41:36 +0200

    Moved commands into -i shell

--- .install.sh
+++ .install.sh
@@ -10,21 +10,22 @@ fi
 #Update/upgrade
 apt-get update && apt-get -y upgrade
 
-# Pull configuration from git
-sudo apt-get install -y build-essential checkinstall git zsh
-sudo -u $SUDO_USER git clone --bare https://github.com/Sawaiz/.dotfiles.git $HOME/.dotfiles
-cd $HOME
-sudo -u $SUDO_USER /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME checkout
-sudo -u $SUDO_USER /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME submodule update --init --recursive
-sudo -u $SUDO_USER /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME remote set-url origin git@github.com:Sawaiz/.dotfiles.git
+# Install packages
+sudo apt-get install -y build-essential checkinstall git zsh tmux
 
-# Install tmux
-sudo apt-get install -y tmux
-
-# Request user to paste RSA Key
-sudo -u $SUDO_USER mkdir -p $HOME/.ssh
-sudo -u $SUDO_USER echo "Replace with id_rsa" > $HOME/.ssh/id_rsa
-sudo -u $SUDO_USER chmod 600 $HOME/.ssh/*
+# Run commands as user
+sudo -u "$SUDO_USER" -i /bin/bash - <<-'EOF'
+  # Pull configuration from git
+  git clone --bare https://github.com/Sawaiz/.dotfiles.git $HOME/.dotfiles
+  cd $HOME
+  /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME checkout
+  /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME submodule update --init --recursive
+  /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME remote set-url origin git@github.com:Sawaiz/.dotfiles.git
+  # Request user to paste RSA Key
+  mkdir -p $HOME/.ssh
+  echo "Replace with id_rsa" > $HOME/.ssh/id_rsa
+  chmod 600 $HOME/.ssh/*
+EOF
 
 # Finished and instrucitons
 echo "Installation finsihed, add your RSA key in ~/.ssh/id_rsa"
